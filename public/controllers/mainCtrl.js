e94acdbbae1356dd12150e3fb8f5cbc06403f23b
Souidan <ahmed.souidan@student.guc.edu.eg>
2016-04-01 21:45:36 +0200

    added classes buttons with controllers and calendar is working--- without minimum date Still: Type on in airports search reservation and sending data to next corresponding date

--- public/controllers/mainCtrl.js
+++ public/controllers/mainCtrl.js
@@ -1,82 +1,81 @@
 /**
  * Main Controller
  */
-App.controller('mainCtrl', function($scope, FlightsSrv, $location) {
 
-  /*----------- Angular Bootstrap Datepicker -----------*/
-  $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
-  $scope.format = $scope.formats[0];
+angular.module('deltaApp').controller('mainCtrl', function($scope) {
 
-  $scope.open1 = function() {
-    $scope.popup1.opened = true;
-  };
+    /*----------- Angular Bootstrap Datepicker -----------*/
+    $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+    $scope.format = $scope.formats[0];
 
-  $scope.open2 = function() {
-    $scope.popup2.opened = true;
-  };
-
-  $scope.setDate = function(year, month, day) {
-    $scope.dt = new Date(year, month, day);
-  };
-
-  $scope.popup1 = {
-    opened: false
-  };
-
-  $scope.popup2 = {
-    opened: false
-  };
-
-
-  /*----------- Angular Bootstrap Typeahead -----------*/
-
-  /* Retrieve List of Airports Codes */
-  function AirportCodes() {
-    FlightsSrv.getAirportCodes().success(function(airports) {
-         $scope.Airports = airports;
-     });
-  };
-
-  /* Record User's Selected Origin Airport  */
-  $scope.SetOriginAirport = function(originAirport) {
-    FlightsSrv.setSelectedOriginAirport(originAirport);
-  };
-
-  /* Record User's Selected Destination Airport  */
-  $scope.SetDestinationAirport = function(destAirport) {
-    FlightsSrv.setSelectedDestinationAirport(destAirport);
-  };
-
-  /* Find All Available Flights  */
-  $scope.SearchFlights = function() {
-    $location.url('/flights');
-  };
-
-  /* Get Airports on page render  */
-  AirportCodes();
-
-});
-
-/*---Angular Classes choices*/
-angular.module('ui.bootstrap.demo').controller('ButtonsCtrl', function ($scope) {
-    $scope.singleModel = 1;
-
-    $scope.radioModel = 'Business Class';
-
-    $scope.checkModel = {
-        left: false,
-        middle: true,
-        right: false
+    $scope.open1 = function () {
+        $scope.popup1.opened = true;
     };
 
-    $scope.checkResults = [];
+    $scope.open2 = function () {
+        $scope.popup2.opened = true;
+    };
 
-    $scope.$watchCollection('checkModel', function () {
+    $scope.setDate = function (year, month, day) {
+        $scope.dt = new Date(year, month, day);
+    };
+
+    $scope.popup1 = {
+        opened: false
+    };
+
+    $scope.popup2 = {
+        opened: false
+    };
+});
+
+
+    /*----------- Angular Bootstrap Typeahead -----------*/
+
+    /* Retrieve List of Airports Codes */
+/*    function AirportCodes() {
+     FlightsSrv.getAirportCodes().success(function(airports) {
+     $scope.Airports = airports;
+     });
+     }
+
+     /!* Record User's Selected Origin Airport  *!/
+     $scope.SetOriginAirport = function(originAirport) {
+     FlightsSrv.setSelectedOriginAirport(originAirport);
+     };
+
+     /!* Record User's Selected Destination Airport  *!/
+     $scope.SetDestinationAirport = function(destAirport) {
+     FlightsSrv.setSelectedDestinationAirport(destAirport);
+     };
+
+     /!* Find All Available Flights  *!/
+     $scope.SearchFlights = function() {
+     $location.url('/flights');
+     };
+
+     /!* Get Airports on page render  *!/
+     AirportCodes();*/
+
+
+    /*---Angular Classes choices*/
+
+    angular.module('deltaApp').controller('buttonsCtrl', function ($scope) {
+
+        $scope.checkModel = {
+            left: false,
+            middle: true,
+            right: false
+        };
+
         $scope.checkResults = [];
-        angular.forEach($scope.checkModel, function (value, key) {
-            if (value) {
-                $scope.checkResults.push(key);
-            }
+
+        $scope.$watchCollection('checkModel', function () {
+            $scope.checkResults = [];
+            angular.forEach($scope.checkModel, function (value, key) {
+                if (value) {
+                    $scope.checkResults.push(key);
+                }
+            });
         });
     });
-});
--- public/core.js
+++ public/core.js
@@ -1,5 +1,5 @@
 /* Create Angular App Instance */
-App = angular.module('deltaApp', ['ui.bootstrap', 'ngRoute']);
+App = angular.module('deltaApp', ['ui.bootstrap', 'ngRoute','ngAnimate']);
 
 /**
  * Angular Routes
