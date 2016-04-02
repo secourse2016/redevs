6695ef0fad4bcb14b3606c05cd7c3b18a6d5a8b6
mkorashi <mariam.korashi@gmail.com>
2016-04-02 20:00:23 +0000

    Modified controller/html models

---
+++ public/controllers/paymentCtrl.js
@@ -0,0 +1,13 @@
+App.controller('MainController', ['$scope', function($scope) { 
+  //$scope
+
+    $scope.name = "";
+    $scope.creditNumber = 0;
+    $scope.CVC=0;
+    $scope.expiryDate= new Date();
+    
+
+  
+  
+  
+}]);
