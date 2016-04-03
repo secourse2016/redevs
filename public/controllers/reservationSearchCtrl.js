964ce2881494910d8dc3bb09b597c3936c588ec8
Jaidaa Yasser <jaidaayasser@gmail.com>
2016-04-03 20:07:50 +0200

    Panel sheyaka

--- public/controllers/reservationSearchCtrl.js
+++ public/controllers/reservationSearchCtrl.js
@@ -11,6 +11,31 @@
 
 App.controller('reservationSearchCtrl', function($scope,$http,reservationSearchSrv) {
 
+  $('dt').addClass('fechado');
+
+var $active = null;
+
+$('dt').click(function(){
+  
+  if ($active !== null){
+    $active.next().slideToggle("fast");
+    $active.removeClass('aberto');
+    $active.addClass('fechado');
+  } 
+  
+  $active = $(this);
+  $active.addClass('aberto');
+  $next = $active.next();
+  
+  if ($next.is(":hidden")){
+    $next.slideToggle("fast");
+  }else{
+    $active.removeClass('aberto');
+    $active.addClass('fechado');
+    $active = null;     
+  }
+  
+})
 
   $scope.reservations=[{
   "reservationCode":"AXY123",
