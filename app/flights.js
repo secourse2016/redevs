a4e80d861f7d71754c4564276ab156baae93278a
Zaher <zaher_tarek@hotmail.com>
2016-04-14 19:04:38 +0200

    reserveTicketMethod still not working

--- app/flights.js
+++ app/flights.js
@@ -40,12 +40,72 @@ db.db().collection('tickets').find( { reservationCode: resNum } ).toArray(cb);
    //         }
 }
 function getTicketsFromJSON() {
-	//return all the tickets/reservation 
+	//return all the tickets/reservation
     return tickets;
 }
+
+
+
+
+function reserveTicket(flights,email,creditCardNumber,adults,children,cb){
+var outgoingflight = getFlightbyId(
+  flights[0].flightNumber,flights[0]
+  .departureDateTime) ;
+  if(flights.length===2)
+  var returnflight = getFlightbyId(
+    flights[1].flightNumber,
+    flights[1].departureDateTime
+  );
+
+
+      for(int j=0;j<adults.length;j++){
+        for(int i =0;i<outgoingflight.seatMap.length,i++){
+          if(outgoingflight.seatMap[i].isReserved==="false")
+            adults[j].seatNumberOutgoing=outgoingflight.seatMap[i].seatNumber;
+            seatMap[i].isReserved="true";
+        }
+      }
+    }
+
+
+
+  db.db().collection('tickets').insertOne({
+    "reservationCode":0,
+    "numberOfAdults":adults.length,
+    "adults":adults,
+    "numberOfChildren":children.length,
+    "children":children,
+    "flights":flights,
+    "email":email,
+    "creditCardNumber":creditCardNumber
+  },function(err,result){
+    assert.equal(err,null);
+    console.log("Reservation done");
+    cb();
+  });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 exports.getTicketsFromDB = getTicketsFromDB;
 exports.getTicketsFromJSON = getTicketsFromJSON;
 exports.reservationSearch = reservationSearch;
-
-
+exports.reserveTicket = reserveTicket;
