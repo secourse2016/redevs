38f0146fe3d29fbb6a6e64d2f72552d0b0d95c96
Mirette <mirette.aziz@hotmail.com>
2016-04-17 16:57:13 +0200

    db logic

---
+++ app/db.js
@@ -0,0 +1,23 @@
+(function(){
+    var client = require('mongodb').MongoClient;
+    var db;
+    //What is our dbURL??
+    
+    module.exports =  {
+        init: function(dbURL, callback) {
+            client.connect(dbURL, function(err, database) {
+              if (err) console.log("[error] mongo connection: ", err);
+              console.log('SUCCESS');
+              db = database;
+              if(callback) callback();
+            });
+        },
+        db: function() {
+            return db;
+        },
+        close: function() {
+            db.close();
+        }
+    };
+
+})();
---
+++ try.js
@@ -0,0 +1,74 @@
+var moment  = require('moment');
+
+
+
+
+var date = moment('2016-04-18T00:00:00+0200');
+      for (var i = 0; i < 44; i++) {
+
+      }
+
+
+
+
+
+
+
+
+
+//var date = moment("2014 04 20", "YYYY MM DD").calendar(date);
+//var date = moment("2014 04 18", "YYYY MM DD");
+      // for (var i = 0; i < 45; i++) {
+      // 	console.log(moment("2014 04 20", "YYYY MM DD").add(i,'days').calendar());
+      // }
+// var date = moment("2014 04 25", "YYYY MM DD");
+// console.log(moment().calendar(date));
+// //console.log(moment().calendar(date)+"T01:32+0200");;
+// //var time = moment().add(2,'hours').calendar(departureDateTime)
+
+
+// var date = moment("2014-04-25T12:30:00+0200");
+// console.log(date.add(2,'hour')._d); //this is working perfectly! 
+
+//console.log(moment("2014-04-25T12:30:00+0200").add(2,'hour')._d); //this is working perfectly! 
+
+// var date = moment("2014-04-25T12:30:00+0200");
+// console.log(date);
+//moment("2014-04-25T12:30:00+0200").add(2,'hour');
+
+//12 AM: 
+
+//console.log(moment('2014-04-25T12:30:00+0200').toDate().getTime());
+//..8000000
+// var date = moment('2016-04-18T00:00:00+0200');
+// 	// for (var i = 1; i < 45; i++) {
+// 	// 	console.log(moment(date).add(i,'hour').add(i*2,'millisecond').toDate().getTime()); 
+// 	// }
+
+// for (var i = 0; i < 44; i++) {
+// 	console.log(moment(date).add(i, 'days').add(,'hour').calendar(date)); 
+// }
+// firstClassSeatMap = [];
+// businessClassSeatMap = [];
+// economyClassSeatMap = [];
+// var counter = 1;
+// var letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//         var counter = 1;
+//         for (var j=0;j<letters.length; j++){
+//           for(var k=1; k<11; k++){
+//             seat = {"seatNumber":letters[j]+""+k, "isReserved":"false"};
+//             if(counter < 11) //0-9 >> first class
+//               firstClassSeatMap.push(seat);
+//             else if(counter >= 11 && counter < 61)
+//               businessClassSeatMap.push(seat);
+//             else if(counter >=61)
+//               economyClassSeatMap.push(seat);
+
+//             counter++;
+//           }
+
+//         }
+
+// //console.log(firstClassSeatMap);
+// console.log(businessClassSeatMap);
+// // console.log(economyClassSeatMap);
