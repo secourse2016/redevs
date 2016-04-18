66a541ceb1b906c1a48247936dd5f2d66c2c4462
Zaher <zaher_tarek@hotmail.com>
2016-04-18 14:36:29 +0200

    flightdetails integrated with api

--- public/controllers/controllerFlightDetails.js
+++ public/controllers/controllerFlightDetails.js
@@ -1,23 +1,29 @@
-App.controller('controllerFlightDetails', function($scope,$http,FlightsSrv,$location,angular-momentjs) {
+App.controller('controllerFlightDetails', function($scope,$http,FlightsSrv,$location) {
 
 $scope.flight = FlightsSrv.getFlights();
 
+if(FlightsSrv.getTripType()=== "RoundTrip"){
 $scope.flightOutgoing = $scope.flight[0];
-$scope.date1=$scope.flightOutgoing.departureDateTime;
-$scope.time1=moment($scope.date1).format('hh:mm');
-$scope.date1=moment($scope.date1).format('YYYY-MM-DD');
+
 
 $scope.flightReturning = $scope.flight[1];
-$scope.date2=$scope.flightReturning.departureDateTime;
-$scope.time2=moment($scope.date2).format('hh:mm');
-$scope.date2=moment($scope.date2).format('YYYY-MM-DD');
+}else{
+	$scope.flightOutgoing = $scope.flight[0];
+	$scope.flightReturning={};
+	$scope.flightReturning.cost = 0;
+	$scope.flightReturning.departureDateTime = "";
+		$scope.flightReturning.arrivalDateTime = "";
+
+
+}
+
 
 
 
 
 
 $scope.proceed = function (){
-
+	FlightsSrv.setTotalCost
 	FlightsSrv.setNumberOfChildren($scope.childrenInput);
 	FlightsSrv.setNumberOfAdults($scope.adultsInput);
 
