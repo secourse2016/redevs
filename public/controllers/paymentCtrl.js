dee63a599bc9271f710e7eace55ad005015dc1c8
Zaher <zaher_tarek@hotmail.com>
2016-04-26 01:15:41 +0200

    angular-stripe completed

--- public/controllers/paymentCtrl.js
+++ public/controllers/paymentCtrl.js
@@ -1,4 +1,4 @@
-App.controller('paymentCtrl',function($scope,$http, FlightsSrv,$location) {
+App.controller('paymentCtrl',function($scope,$http, FlightsSrv,$location,stripe) {
   //$scope
 
     $scope.name = "";
@@ -8,34 +8,55 @@ App.controller('paymentCtrl',function($scope,$http, FlightsSrv,$location) {
     $scope.year=0;
 
 
+
+
+
   $scope.submit = function(){
-     FlightsSrv.setName($scope.name);
-     FlightsSrv.setCreditCardNumber($scope.creditNumber);
-     FlightsSrv.setCVC($scope.CVC);
-     FlightsSrv.setMonth($scope.month);
-     FlightsSrv.setYear($scope.year);
+    //  FlightsSrv.setName($scope.name);
+    //  FlightsSrv.setCreditCardNumber($scope.creditNumber);
+    //  FlightsSrv.setCVC($scope.CVC);
+    //  FlightsSrv.setMonth($scope.month);
+    //  FlightsSrv.setYear($scope.year);
+    stripe.card.createToken({
+        number: $scope.creditNumber,
+        cvc : $scope.CVC,
+        exp_month:$scope.month,
+        exp_year:$scope.year
+    },function(status,response){
+      if(response.error){
+        console.log(response.error);
+        console.log($scope.year);
+      }else{
+      var data = {
+           		tripType:FlightsSrv.getTripType(),
+          		flights:FlightsSrv.getFlights(),
+          		adults:FlightsSrv.getAdultsInfo(),
+          		children:FlightsSrv.getChildrenInfo(),
+          		classs:FlightsSrv.getClass(),
+              token:response.id
+
+          	};
+
+            FlightsSrv.postReservation(data).success(function(response,status){
+            FlightsSrv.setReservationNumber(response.time);
+             $location.url('/thankYou');
+            });
+
+      }
+    });
 
 
 
 
 
-	var data = {
-       		tripType:FlightsSrv.getTripType(),
-      		flights:FlightsSrv.getFlights(),
-      		adults:FlightsSrv.getAdultsInfo(),
-      		children:FlightsSrv.getChildrenInfo(),
-      		creditCardNumber:"123",
-      		classs:FlightsSrv.getClass()
-      	};
-
-
-      FlightsSrv.postReservation(data).success(function(response,status){
-      FlightsSrv.setReservationNumber(response.time);
-      });
 
 
 
- $location.url('/thankYou');
+
+
+
+
+
 
 
 
